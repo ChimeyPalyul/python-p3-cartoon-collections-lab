7c0fec2e03193ff745a9dc38da57a8fc2d35ba0f
Chimey Palyul <palyul.chimey@gmail.com>
2023-10-23 15:52:53 -0600

    f'in cheesefinder

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+CHEESES=["cheddar", "gouda", "camembert"]
+def roll_call_dwarves(dwarves):
+    i=1
+    for dwarf in dwarves:
+        print(f"{i}. {dwarf}")
+        i += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return[f"{call.title()}!" for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+        if len(word) > 4:
+            return True
+        
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    for food in foods:
+        if food in CHEESES:
+            return food
+
+    return None
